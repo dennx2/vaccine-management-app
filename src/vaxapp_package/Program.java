20e81118d84ef3c070245094e9a9000b819ab362
Jaydenn Chang <75432211+dennx2@users.noreply.github.com>
2023-03-19 12:16:35 -0400

    Files uploaded

---
+++ src/vaxapp_package/Program.java
@@ -0,0 +1,28 @@
+package vaxapp_package;
+
+import javax.swing.*;
+import java.util.ArrayList;
+
+/**
+ * Student Name: Kaiyan Chen, Simul Bista, Jaydenn(Ching-Ting) Chang
+ * Student ID: N01489178, N01489966, N01511476
+ * Section: ITC-5201-RIA
+ */
+
+public class Program {
+	public static void main(String args[]) throws Exception {
+		JFrame frame = new VaxApp();
+		frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		frame.setVisible(true);
+//		ArrayList<String> msg = new ArrayList<String>();
+//		Recorder r1 = new Recorder();
+		// mm/dd/yyy format
+//		String d_value = "04/06/2022";
+//		String city_name = "Ottawa";
+//		Integer pfizer = 200;
+//		Integer moderna=300;
+//		Integer astrazeneca=400;
+//		System.out.println(Recorder.submitRecord(d_value, city_name, pfizer, moderna, astrazeneca));
+//		System.out.println(Recorder.searchData("Ottawa", "04/06/2022"));
+	}
+}
